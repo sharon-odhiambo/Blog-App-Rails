a54b6d5755aa824caf188cf9e3c051005aab296a
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-10-27 21:51:55 +0300

    Correct linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
-    belongs_to:users,  class_name: 'User'
-    belongs_to:posts
+  belongs_to :users, class_name: 'User'
+  belongs_to :posts
 
-    def update_comments_counter
-        Post.update(comments_counter: Post.first.comments.count)
-    end
+  def update_comments_counter
+    Post.update(comments_counter: Post.first.comments.count)
+  end
 end
