6db210b4406bf9635adaa9e125ac3416377985e2
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-10-27 16:29:51 +0300

    add associationa, migrations and insert data to users, posts and comments

--- db/migrate/20221027095752_create_users.rb
+++ db/migrate/20221027095752_create_users.rb
@@ -1,10 +1,10 @@
-class CreateUsers < ActiveRecord::Migration[7.0]
+class CreateUser < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.text :name
       t.text :photo
       t.text :bio
-      t.integer :posts_counter
+      t.integer :postscounter
 
       t.timestamps
     end
