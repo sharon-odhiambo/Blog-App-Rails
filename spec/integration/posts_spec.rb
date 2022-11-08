8e3762d3b51f5cfa9013209611c8e5256ee28dd9
stevemwenje <stephen.mwenje@equitybank.co.ke>
2022-11-08 21:36:00 +0300

    add posts integration specs for index and show views

---
+++ spec/integration/posts_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts Page', type: :view do
+  describe 'posts#index page' do
+  before(:each) do
+    @user1 = User.create(name: "Sharon",
+    photo: "https://images.unsplash.com/photo-1667255949472-2f8bba82deb1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80\n  ",
+bio: "oooooooo",
+posts_counter: 0)
+@post1 = Post.create(
+title: "Hello",
+text: "This is my first post",
+comments_counter: 14,
+likes_counter: 6,
+user_id: @user1.id)
+visit user_posts_path(@user1)
+    end
+    it 'shows the right user profile picture' do
+      expect(page).to have_css("img[src='#{@user1.photo}']")
+    end
+    it 'shows the right user name' do
+        expect(page).to have_content(@user1.name)
+      end
+    it 'shows the right user posts counter' do
+        expect(body).to have_content("Number of Posts: 1")
+      end
+    it 'shows the right user post title' do
+        expect(page).to have_content("Post :#{@post1.id}")
+      end
+    it 'shows the right user post body' do
+        expect(page).to have_content(@post1.text)
+      end
+    it 'shows the user post 1 comments' do
+        expect(page).to have_content(@post1.comments_counter)
+      end
+    it 'shows the user post 1 likes' do
+        expect(page).to have_content(@post1.likes_counter)
+      end
+    it 'redirects the user to the right post' do
+        expect(page).to have_link(href: user_post_path(@user1, @post1))
+    end
+  end
+  describe 'pposts#show page' do
+  before(:each) do
+    @user1 = User.create(name: "Sharon",
+                        photo: "https://images.unsplash.com/photo-1667255949472-2f8bba82deb1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80\n  ",
+    bio: "oooooooo",
+    posts_counter: 10)
+    @post1 = Post.create(
+    title: "Hello",
+    text: "This is my first post",
+    comments_counter: 14,
+    likes_counter: 6,
+    user_id: @user1.id)
+    @comment = Comment.create(post: @post1, user: @user1, text: "Hi Tom!")
+    visit user_post_path(@user1, @post1)
+  end
+  it 'shows the right title of the post' do
+    expect(page).to have_content(@post1.title)
+  end
+  it 'shows the right username of who added the post' do
+    expect(page).to have_content(@post1.user.name)
+  end
+  it 'shows the right number of comments in a post' do
+    expect(page).to have_content(@post1.comments_counter)
+  end
+  it 'shows the right number of likes in a post' do
+    expect(page).to have_content(@post1.likes_counter)
+  end
+  it 'shows the right body of the post' do
+    expect(page).to have_content(@post1.text)
+  end
+  it 'shows the right username of a commentor' do
+    expect(page).to have_content(@comment.user.name)
+  end
+  it 'shows the right comment by a commentor' do
+    expect(page).to have_content(@comment.text)
+  end
+end
+end
--- spec/integration/users_spec.rb
+++ spec/integration/users_spec.rb
@@ -1,11 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Users index', type: :system do
+  describe 'users#index page' do
   before(:each) do
     @user1 = User.create(name: "Sharon",
                         photo: "https://images.unsplash.com/photo-1667857481501-b447de8ed0c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60",
-    bio: "Teacher from Kenya",
-    posts_counter: 3)
+    bio: "Teacher from Kenya")
+    @user2 = User.create(name: "Tom",
+    photo:  "https://images.unsplash.com/photo-1667857481501-b447de8ed0c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60",
+    bio: "Teacher from Mexico",
+    posts_counter: 0)
+    visit users_path
+  end
+    it 'shows the right user name' do
+      expect(page).to have_content(@user1.name)
+      expect(page).to have_content(@user2.name)
+    end
+    it 'shows the right user profile picture' do
+        expect(page).to have_css("img[src='#{@user1.photo}']")
+        expect(page).to have_css("img[src='#{@user2.photo}']")
+      end
+    it 'redirects the user to the right user information' do
+        expect(page).to have_link(@user1.name, href: user_path(@user1.id))
+    end
+    it 'redirects the user to the right post' do
+        expect(page).to have_link(href: user_path(@user1.id))
+    end
+  end
+  describe 'users#show page' do
+  before(:each) do
+    @user1 = User.create(name: "Sharon",
+                        photo: "https://images.unsplash.com/photo-1667857481501-b447de8ed0c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60",
+    bio: "Teacher from Kenya")
     @user2 = User.create(name: "Tom",
     photo:  "https://images.unsplash.com/photo-1667857481501-b447de8ed0c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60",
     bio: "Teacher from Mexico",
@@ -16,49 +42,26 @@ RSpec.describe 'Users index', type: :system do
     comments_counter: 1,
     likes_counter: 1,
     user_id: 1)
-  end
-  describe 'users#index page' do
-    it 'shows the right user name' do
-      visit users_path
-      expect(page).to have_content(@user1.name)
-      expect(page).to have_content(@user2.name)
-    end
-    it 'shows the right user profile picture' do
-        visit users_path
-        expect(page).to have_css("img[src='#{@user1.photo}']")
-        expect(page).to have_css("img[src='#{@user2.photo}']")
-      end
-    it 'redirects the user to the right post' do
-        visit users_path
-        expect(page).to have_link(href: user_path(@user1.id))
-    end
-  end
-  describe 'users#show page' do
-  it 'shows the right user1 name, bio, profile picture and number of posts' do
     visit user_path(@user1)
+  end
+  it 'shows the right user1 name, bio, profile picture and number of posts' do
     expect(page).to have_content(@user1.name)
     expect(page).to have_content(@user1.bio)
     expect(page).to have_css("img[src='#{@user1.photo}']")
-    expect(page).to have_content(@user1.posts_counter)
+    expect(page).to have_content("Number of Posts: #{@user1.posts_counter}")
   end
   it 'shows the right user2 name, bio, profile picture and number of posts' do
     visit user_path(@user2)
     expect(page).to have_content(@user2.name)
     expect(page).to have_content(@user2.bio)
     expect(page).to have_css("img[src='#{@user2.photo}']")
-    expect(page).to have_content(@user2.posts_counter)
+    expect(page).to have_content("Number of Posts: #{@user2.posts_counter}")
   end
   it 'shows the user most recent three posts' do
-    visit user_path(@user1)
     expect(@user1.most_recent_posts.count).to be_between(0, 3)
   end
   it 'shows the a button for viewing user posts' do
-    visit user_path(@user1)
     expect(page).to have_link('See all Posts', href: user_posts_path(@user1))
   end
-  # it 'shows the a button for viewing user post show pge' do
-  #   visit user_path(@user1)
-  #   expect(page).to have_link('Post:2', href: 'users/3/posts/2')
-  # end
   end
 end
