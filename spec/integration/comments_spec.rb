80871b1fde5dfbabf500b6d68d842b871d1b857a
sharon-odhiambo <sharon.odhiambo100@gmail.com>
2022-11-12 16:22:26 +0300

    add api endpoint documentation for viewing posts, comments and creating comments

---
+++ spec/integration/comments_spec.rb
@@ -0,0 +1,57 @@
+require 'swagger_helper'
+
+describe 'Comments API' do
+
+  path '/api/users/:id/posts/:id/comments' do
+
+    post 'Creates a comment' do
+      tags 'Comments'
+      consumes 'application/json', 'application/xml'
+      parameter user: :post, in: :body, schema: {
+        type: :object,
+        properties: {
+          user_id: { type: :integer },
+          post_id: { type: :integer },
+          text: { type: :string }
+        },
+        required: [ 'user_id', 'post_id', 'text' ]
+      }
+
+      response '201', 'pet created' do
+        let(:comment) { { user_id: 1, post_id: 1, text: 'Awesome stuff' } }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:comment) { { text: 'Wow' } }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/users/:id/posts/:id/comments' do
+
+    get 'Retrieves a comment' do
+      tags 'Comments'
+      produces 'application/json', 'application/xml'
+      parameter user: :post, :in => :path, :type => :integer
+
+      response '200', 'name found' do
+        schema type: :object,
+          properties: {
+            user_id: { type: :integer, },
+            post_id: { type: :integer }
+          },
+          required: [ 'user_id', 'post_id' ]
+
+        let(:comment) { Comment.create(user_id: 1,post_id: 1, text: 'Hi there') }
+        run_test!
+      end
+
+      response '404', 'pet not found' do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+    end
+  end
+end
---
+++ spec/integration/posts_spec.rb
@@ -0,0 +1,29 @@
+require 'swagger_helper'
+
+describe 'Users API' do
+
+  path '/api/users/:id/posts' do
+
+    get 'Displays a user posts' do
+      tags 'Posts'
+      consumes 'application/json', 'application/xml'
+      parameter user: :id, in: :body, schema: {
+        type: :object,
+        properties: {
+          id: { type: :integer }
+        },
+        required: [ 'id' ]
+      }
+
+      response '201', 'post created' do
+        let(:post) { { id: 1 } }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:post) { { id: 3 } }
+        run_test!
+      end
+    end
+  end
+end
